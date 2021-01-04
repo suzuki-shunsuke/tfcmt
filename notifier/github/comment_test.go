ea2fce6cdd09b98f34b5d5f2456164396478e5c0
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-01-04 22:59:40 +0900

    refactor: remove unused code

--- notifier/github/comment_test.go
+++ notifier/github/comment_test.go
@@ -2,10 +2,7 @@ package github
 
 import (
 	"context"
-	"reflect"
 	"testing"
-
-	"github.com/google/go-github/github"
 )
 
 func TestCommentPost(t *testing.T) {
@@ -64,61 +61,6 @@ func TestCommentPost(t *testing.T) {
 		err = client.Comment.Post(context.Background(), testCase.body, testCase.opt)
 		if (err == nil) != testCase.ok {
 			t.Errorf("got error %q", err)
-		}
-	}
-}
-
-func TestCommentList(t *testing.T) {
-	t.Parallel()
-	comments := []*github.IssueComment{
-		{
-			ID:   github.Int64(371748792),
-			Body: github.String("comment 1"),
-		},
-		{
-			ID:   github.Int64(371765743),
-			Body: github.String("comment 2"),
-		},
-	}
-	testCases := []struct {
-		config   Config
-		number   int
-		ok       bool
-		comments []*github.IssueComment
-	}{
-		{
-			config:   newFakeConfig(),
-			number:   1,
-			ok:       true,
-			comments: comments,
-		},
-		{
-			config:   newFakeConfig(),
-			number:   12,
-			ok:       true,
-			comments: comments,
-		},
-		{
-			config:   newFakeConfig(),
-			number:   123,
-			ok:       true,
-			comments: comments,
-		},
-	}
-
-	for _, testCase := range testCases {
-		client, err := NewClient(context.Background(), testCase.config)
-		if err != nil {
-			t.Fatal(err)
-		}
-		api := newFakeAPI()
-		client.API = &api
-		comments, err := client.Comment.List(context.Background(), testCase.number)
-		if (err == nil) != testCase.ok {
-			t.Errorf("got error %q", err)
-		}
-		if !reflect.DeepEqual(comments, testCase.comments) {
-			t.Errorf("got %v but want %v", comments, testCase.comments)
 		}
 	}
 }
