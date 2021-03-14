a49c67cc4acd7e08684a908ee652a0284fde8c53
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-03-14 22:39:57 +0900

    refactor: refactor platform.Get

--- pkg/platform/ci.go
+++ pkg/platform/ci.go
@@ -5,40 +5,24 @@ import (
 	"os"
 )
 
-func Get(ciname string) string {
+func GetLink(ciname string) string {
 	switch ciname {
 	case "circleci", "circle-ci":
-		return circleci()
+		return os.Getenv("CIRCLE_BUILD_URL")
 	case "codebuild":
-		return codebuild()
+		return os.Getenv("CODEBUILD_BUILD_URL")
 	case "github-actions":
-		return githubActions()
+		return fmt.Sprintf(
+			"https://github.com/%s/actions/runs/%s",
+			os.Getenv("GITHUB_REPOSITORY"),
+			os.Getenv("GITHUB_RUN_ID"),
+		)
 	case "cloud-build", "cloudbuild":
-		return cloudbuild()
+		return fmt.Sprintf(
+			"https://console.cloud.google.com/cloud-build/builds/%s?project=%s",
+			os.Getenv("BUILD_ID"),
+			os.Getenv("PROJECT_ID"),
+		)
 	}
 	return ""
-}
-
-func circleci() string {
-	return os.Getenv("CIRCLE_BUILD_URL")
-}
-
-func codebuild() string {
-	return os.Getenv("CODEBUILD_BUILD_URL")
-}
-
-func githubActions() string {
-	return fmt.Sprintf(
-		"https://github.com/%s/actions/runs/%s",
-		os.Getenv("GITHUB_REPOSITORY"),
-		os.Getenv("GITHUB_RUN_ID"),
-	)
-}
-
-func cloudbuild() string {
-	return fmt.Sprintf(
-		"https://console.cloud.google.com/cloud-build/builds/%s?project=%s",
-		os.Getenv("BUILD_ID"),
-		os.Getenv("PROJECT_ID"),
-	)
 }
