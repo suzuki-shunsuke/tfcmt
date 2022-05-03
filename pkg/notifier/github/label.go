3d9f77ecf91b644224212532b3b24bd82efacd1a
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-05-03 14:44:55 +0900

    refactor: split files

---
+++ pkg/notifier/github/label.go
@@ -0,0 +1,118 @@
+package github
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/go-github/v39/github"
+	"github.com/sirupsen/logrus"
+	"github.com/suzuki-shunsuke/tfcmt/pkg/terraform"
+)
+
+func (g *NotifyService) updateLabels(ctx context.Context, result terraform.ParseResult) []string { //nolint:cyclop
+	cfg := g.client.Config
+	var (
+		labelToAdd string
+		labelColor string
+	)
+
+	switch {
+	case result.HasAddOrUpdateOnly:
+		labelToAdd = cfg.ResultLabels.AddOrUpdateLabel
+		labelColor = cfg.ResultLabels.AddOrUpdateLabelColor
+	case result.HasDestroy:
+		labelToAdd = cfg.ResultLabels.DestroyLabel
+		labelColor = cfg.ResultLabels.DestroyLabelColor
+	case result.HasNoChanges:
+		labelToAdd = cfg.ResultLabels.NoChangesLabel
+		labelColor = cfg.ResultLabels.NoChangesLabelColor
+	case result.HasPlanError:
+		labelToAdd = cfg.ResultLabels.PlanErrorLabel
+		labelColor = cfg.ResultLabels.PlanErrorLabelColor
+	}
+
+	errMsgs := []string{}
+
+	logE := logrus.WithFields(logrus.Fields{
+		"program": "tfcmt",
+	})
+
+	currentLabelColor, err := g.removeResultLabels(ctx, labelToAdd)
+	if err != nil {
+		msg := "remove labels: " + err.Error()
+		logE.WithError(err).Error("remove labels")
+		errMsgs = append(errMsgs, msg)
+	}
+
+	if labelToAdd == "" {
+		return errMsgs
+	}
+
+	if currentLabelColor == "" {
+		labels, _, err := g.client.API.IssuesAddLabels(ctx, cfg.PR.Number, []string{labelToAdd})
+		if err != nil {
+			msg := "add a label " + labelToAdd + ": " + err.Error()
+			logE.WithError(err).WithFields(logrus.Fields{
+				"label": labelToAdd,
+			}).Error("add a label")
+			errMsgs = append(errMsgs, msg)
+		}
+		if labelColor != "" {
+			// set the color of label
+			for _, label := range labels {
+				if labelToAdd == label.GetName() {
+					if label.GetColor() != labelColor {
+						if _, _, err := g.client.API.IssuesUpdateLabel(ctx, labelToAdd, labelColor); err != nil {
+							msg := "update a label color (name: " + labelToAdd + ", color: " + labelColor + "): " + err.Error()
+							logE.WithError(err).WithFields(logrus.Fields{
+								"label": labelToAdd,
+								"color": labelColor,
+							}).Error("update a label color")
+							errMsgs = append(errMsgs, msg)
+						}
+					}
+				}
+			}
+		}
+	} else if labelColor != "" && labelColor != currentLabelColor {
+		// set the color of label
+		if _, _, err := g.client.API.IssuesUpdateLabel(ctx, labelToAdd, labelColor); err != nil {
+			msg := "update a label color (name: " + labelToAdd + ", color: " + labelColor + "): " + err.Error()
+			logE.WithError(err).WithFields(logrus.Fields{
+				"label": labelToAdd,
+				"color": labelColor,
+			}).Error("update a label color")
+			errMsgs = append(errMsgs, msg)
+		}
+	}
+	return errMsgs
+}
+
+func (g *NotifyService) removeResultLabels(ctx context.Context, label string) (string, error) {
+	cfg := g.client.Config
+	// A Pull Request can have 100 labels the maximum
+	labels, _, err := g.client.API.IssuesListLabels(ctx, cfg.PR.Number, &github.ListOptions{
+		PerPage: 100, //nolint:gomnd
+	})
+	if err != nil {
+		return "", err
+	}
+
+	labelColor := ""
+	for _, l := range labels {
+		labelText := l.GetName()
+		if labelText == label {
+			labelColor = l.GetColor()
+			continue
+		}
+		if cfg.ResultLabels.IsResultLabel(labelText) {
+			resp, err := g.client.API.IssuesRemoveLabel(ctx, cfg.PR.Number, labelText)
+			// Ignore 404 errors, which are from the PR not having the label
+			if err != nil && resp.StatusCode != http.StatusNotFound {
+				return labelColor, err
+			}
+		}
+	}
+
+	return labelColor, nil
+}
