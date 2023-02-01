78cfe0b4a1cfc42faa814cdfdaba30cf5f511190
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-02-01 20:16:08 +0900

    chore: update go-github to v50

--- pkg/notifier/github/commits.go
+++ pkg/notifier/github/commits.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 
-	"github.com/google/go-github/v49/github"
+	"github.com/google/go-github/v50/github"
 )
 
 // CommitsService handles communication with the commits related
