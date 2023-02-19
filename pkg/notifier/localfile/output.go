59a98cfa1fbb31ebfee1899dcb084bdea6c89265
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-02-19 10:28:19 +0900

    fix(localfile): append the output to a file instead of overwriting

--- pkg/notifier/localfile/output.go
+++ pkg/notifier/localfile/output.go
@@ -8,16 +8,18 @@ import (
 
 type OutputService service
 
+const filePermission os.FileMode = 0o644
+
 // WriteToFile Write result to file
 func (f *OutputService) WriteToFile(ctx context.Context, body string, outputFile string) error {
-	file, err := os.Create(outputFile)
+	file, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, filePermission)
 	if err != nil {
-		return fmt.Errorf("create a file to output the result to a file: %w", err)
+		return fmt.Errorf("open a file to output the result to a file: %w", err)
 	}
 
 	defer file.Close()
 
-	if _, err := file.WriteString(body); err != nil {
+	if _, err := file.WriteString(body + "\n"); err != nil {
 		return fmt.Errorf("write the result to a file: %w", err)
 	}
 	return nil
