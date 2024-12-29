f45bec967de19bdf44a7fb4605a0f129f3aa8093
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-12-29 13:23:18 +0900

    refactor: use suzuki-shunsuke/gen-go-jsonschema (#1553)

--- cmd/gen-jsonschema/main.go
+++ cmd/gen-jsonschema/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
-	"github.com/invopop/jsonschema"
+	"github.com/suzuki-shunsuke/gen-go-jsonschema/jsonschema"
 	"github.com/suzuki-shunsuke/tfcmt/v4/pkg/config"
 )
 
@@ -18,27 +15,8 @@ func main() {
 }
 
 func core() error {
-	if err := gen(&config.Config{}, "json-schema/tfcmt.json"); err != nil {
-		return err
-	}
-	return nil
-}
-
-func gen(input interface{}, p string) error {
-	f, err := os.Create(p)
-	if err != nil {
-		return fmt.Errorf("create a file %s: %w", p, err)
-	}
-	defer f.Close()
-	encoder := json.NewEncoder(f)
-	encoder.SetIndent("", "  ")
-	s := jsonschema.Reflect(input)
-	b, err := json.MarshalIndent(s, "", "  ")
-	if err != nil {
-		return fmt.Errorf("mashal schema as JSON: %w", err)
-	}
-	if err := os.WriteFile(p, []byte(strings.ReplaceAll(string(b), "http://json-schema.org", "https://json-schema.org")+"\n"), 0o644); err != nil { //nolint:gosec,mnd
-		return fmt.Errorf("write JSON Schema to %s: %w", p, err)
+	if err := jsonschema.Write(&config.Config{}, "json-schema/tfcmt.json"); err != nil {
+		return fmt.Errorf("create or update a JSON Schema: %w", err)
 	}
 	return nil
 }
