4aa469552119953696e2b0544ff88ae9ed1218c5
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-03-25 12:59:38 +0900

    refactor: nosnakecase was removed

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -130,7 +130,7 @@ func createDummy(file string) {
 	if _, err := os.Stat(file); err == nil {
 		return
 	}
-	f, err := os.OpenFile(file, os.O_RDONLY|os.O_CREATE, 0o666) //nolint:nosnakecase
+	f, err := os.OpenFile(file, os.O_RDONLY|os.O_CREATE, 0o666)
 	if err != nil {
 		panic(err)
 	}
