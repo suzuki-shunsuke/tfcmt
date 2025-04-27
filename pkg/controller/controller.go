9cf75d00272a72fe72d0d3050a42784e1f1b4b5b
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-04-27 13:02:45 +0900

    refactor: simplify code

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -133,18 +133,6 @@ func (c *Controller) getNotifier(ctx context.Context) (notifier.Notifier, error)
 	}
 	// Write output to file instead of github comment
 	if c.Config.Output != "" {
-		var githubLabelConfig *localfile.GitHubLabelConfig
-		if !c.Config.Terraform.Plan.DisableLabel {
-			githubLabelConfig = &localfile.GitHubLabelConfig{
-				BaseURL:         c.Config.GHEBaseURL,
-				GraphQLEndpoint: c.Config.GHEGraphQLEndpoint,
-				Owner:           c.Config.CI.Owner,
-				Repo:            c.Config.CI.Repo,
-				PRNumber:        c.Config.CI.PRNumber,
-				Revision:        c.Config.CI.SHA,
-				Labels:          labels,
-			}
-		}
 		client, err := localfile.NewClient(&localfile.Config{
 			OutputFile:         c.Config.Output,
 			Parser:             c.Parser,
@@ -157,7 +145,15 @@ func (c *Controller) getNotifier(ctx context.Context) (notifier.Notifier, error)
 			Templates:          c.Config.Templates,
 			Masks:              c.Config.Masks,
 			DisableLabel:       c.Config.Terraform.Plan.DisableLabel,
-			GitHubLabelConfig:  githubLabelConfig,
+			GitHubLabelConfig: &localfile.GitHubLabelConfig{
+				BaseURL:         c.Config.GHEBaseURL,
+				GraphQLEndpoint: c.Config.GHEGraphQLEndpoint,
+				Owner:           c.Config.CI.Owner,
+				Repo:            c.Config.CI.Repo,
+				PRNumber:        c.Config.CI.PRNumber,
+				Revision:        c.Config.CI.SHA,
+				Labels:          labels,
+			},
 		})
 		if err != nil {
 			return nil, err
